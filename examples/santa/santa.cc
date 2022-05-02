04f425b36abf271bf06655e34602017146f4c988
Luke Cheeseman <luke_cheeseman@hotmail.co.uk>
2022-05-02 14:57:51 +0100

    tidying up santa example

--- examples/santa/santa.cc
+++ examples/santa/santa.cc
@@ -5,44 +5,49 @@
 #include <cpp/when.h>
 
 namespace SantaProblem {
+  struct Santa {
+    int count;
+    Santa(int count): count(count) {};
+  };
   struct Reindeer {};
   struct Elf {};
 
   template<typename T>
-  using Group = std::vector<T>;
+  using Imm = std::shared_ptr<const T>;
 
   template<typename T>
-  using WaitingGroups = std::vector<Group<T>>;
+  using Group = std::queue<std::unique_ptr<T>>;
+
+  template<typename T>
+  using WaitingGroups = std::queue<Group<T>>;
 
   template<typename T>
   struct EntityInfo {
-    cown_ptr<Group<std::unique_ptr<T>>> group;
-    cown_ptr<WaitingGroups<std::unique_ptr<T>>> waiting;
+    cown_ptr<Group<T>> group;
+    cown_ptr<WaitingGroups<T>> waiting;
     size_t threshold;
 
-    EntityInfo(size_t threshold): group(make_cown<Group<std::unique_ptr<T>>>()), waiting(make_cown<WaitingGroups<std::unique_ptr<T>>>()), threshold(threshold) {}
+    EntityInfo(size_t threshold): group(make_cown<Group<T>>()), waiting(make_cown<WaitingGroups<T>>()), threshold(threshold) {}
   };
 
   struct Workshop {
-    cown_ptr<int> santa; // santa is just a job count here
-
-    std::shared_ptr<const EntityInfo<Reindeer>> reindeer_info;
-    std::shared_ptr<const EntityInfo<Elf>> elf_info;
+    cown_ptr<Santa> santa; // santa is just a job count here
+    Imm<EntityInfo<Reindeer>> reindeer_info;
+    Imm<EntityInfo<Elf>> elf_info;
 
     template<typename T>
-    static void add_entity(std::shared_ptr<const Workshop> ws, std::shared_ptr<const EntityInfo<T>> info, std::unique_ptr<T> entity) {
-      when(info->group) << [ws, info, entity = std::move(entity)](acquired_cown<Group<std::unique_ptr<T>>> group) mutable {
-        group->push_back(std::move(entity));
+    static void add_entity(Imm<Workshop> ws, Imm<EntityInfo<T>> info, std::unique_ptr<T> entity) {
+      when(info->group) << [ws, info, entity = move(entity)](acquired_cown<Group<T>> group) mutable {
+        group->push(move(entity));
         if (group->size() >= info->threshold) {
-          Group<std::unique_ptr<T>> sg;
-          auto it = group->begin();
+          Group<T> sg;
           size_t i = 0;
           while (i++ < info->threshold) {
-            sg.push_back(std::move(*it));
-            it = group->erase(it);
+            sg.push(move(group->front()));
+            group->pop();
           }
-          when(info->waiting) << [sg = std::move(sg), ws](acquired_cown<WaitingGroups<std::unique_ptr<T>>> waiting) mutable {
-            waiting->push_back(std::move(sg));
+          when(info->waiting) << [sg = move(sg), ws](acquired_cown<WaitingGroups<T>> waiting) mutable {
+            waiting->push(move(sg));
             Workshop::process(ws);
           };
         }
@@ -50,40 +55,37 @@ namespace SantaProblem {
     }
 
     template<typename T>
-    static void return_entities(std::shared_ptr<const Workshop> ws, std::shared_ptr<const EntityInfo<T>> info, Group<std::unique_ptr<T>> entities) {
-      auto it = entities.begin();
-      while (it != entities.end()) {
-        Workshop::add_entity(ws, info, std::move(*it));
-        it = entities.erase(it);
+    static void return_entities(Imm<Workshop> ws, Imm<EntityInfo<T>> info, Group<T> entities) {
+      while (!entities.empty()) {
+        Workshop::add_entity(ws, info, move(entities.front()));
+        entities.pop();
       }
     }
 
-    static void process(std::shared_ptr<const Workshop> ws) {
-      when(ws->santa, ws->reindeer_info->waiting, ws->elf_info->waiting) << [ws](acquired_cown<int> santa, acquired_cown<WaitingGroups<std::unique_ptr<Reindeer>>> waiting_reindeer, acquired_cown<WaitingGroups<std::unique_ptr<Elf>>> waiting_elves){
-        if((*santa)-- > 0) {
+    static void process(Imm<Workshop> ws) {
+      when(ws->santa, ws->reindeer_info->waiting, ws->elf_info->waiting) << [ws](acquired_cown<Santa> santa, acquired_cown<WaitingGroups<Reindeer>> waiting_reindeer, acquired_cown<WaitingGroups<Elf>> waiting_elves){
+        if((santa->count)-- > 0) {
           if(!waiting_reindeer->empty()) {
-            auto reindeer = waiting_reindeer->begin();
             std::cout << "Reindeer and Santa meet to work" << std::endl;
-            Workshop::return_entities<Reindeer>(ws, ws->reindeer_info, std::move(*reindeer));
-            waiting_reindeer->erase(reindeer);
+            Workshop::return_entities<Reindeer>(ws, ws->reindeer_info, move(waiting_reindeer->front()));
+            waiting_reindeer->pop();
           } else if (!waiting_elves->empty()){
-            auto elves = waiting_elves->begin();
             std::cout << "Elves and Santa meet to work" << std::endl;
-            Workshop::return_entities<Elf>(ws, ws->elf_info, std::move(*elves));
-            waiting_elves->erase(elves);
+            Workshop::return_entities<Elf>(ws, ws->elf_info, move(waiting_elves->front()));
+            waiting_elves->pop();
           } else {
-            check(false && "we should not be able to get here");
+            check(false && "we should not having pending processes without work available");
           }
         }
       };
     }
 
-    Workshop(): santa(make_cown<int>(int{50})),
-                reindeer_info(std::make_shared<EntityInfo<Reindeer>>(9)),
-                elf_info(std::make_shared<EntityInfo<Elf>>(3)) {}
+    Workshop(): santa(make_cown<Santa>(50)),
+                reindeer_info(std::make_shared<const EntityInfo<Reindeer>>(9)),
+                elf_info(std::make_shared<const EntityInfo<Elf>>(3)) {}
 
     static void create() {
-      const std::shared_ptr<const Workshop> ws = std::make_shared<const Workshop>();
+      Imm<Workshop> ws = std::make_shared<const Workshop>();
 
       for (size_t i = 0; i < 9; ++i) {
         Workshop::add_entity<Reindeer>(ws, ws->reindeer_info, std::make_unique<Reindeer>());
