d75df771b6b9e515471f838ce5b65e19fe1053d6
Luke Cheeseman <luke_cheeseman@hotmail.co.uk>
2022-06-09 16:19:59 +0100

    adding some variants of boids algorithm

--- examples/boids/boids.cpp
+++ examples/boids/boids.cpp
@@ -16,7 +16,7 @@ struct Vector {
     y += other.y;
   }
 
-  Vector operator+(Vector other) {
+  Vector operator+(const Vector other) const {
     Vector v{x, y};
     v += other;
     return v;
@@ -27,7 +27,7 @@ struct Vector {
     y /= scalar;
   }
 
-  Vector operator/(double scalar) {
+  Vector operator/(const double scalar) {
     Vector v{x, y};
     v /= scalar;
     return v;
@@ -38,7 +38,7 @@ struct Vector {
     y *= scalar;
   }
 
-  Vector operator*(double scalar) {
+  Vector operator*(const double scalar) const {
     Vector v{x, y};
     v *= scalar;
     return v;
@@ -49,7 +49,7 @@ struct Vector {
     y -= other.y;
   }
 
-  Vector operator-(Vector other){
+  Vector operator-(const Vector other) const {
     Vector v{x, y};
     v -= other;
     return v;
@@ -64,8 +64,8 @@ struct Vector {
 
 std::ostream& operator<<(std::ostream& os, const Vector& v)
 {
-    os << "{" << v.x << ", " << v.y << "}";
-    return os;
+  os << "{" << v.x << ", " << v.y << "}";
+  return os;
 }
 
 struct Boid {
@@ -73,174 +73,266 @@ struct Boid {
   Vector velocity;
 
   Boid(Vector position) : position(position), velocity(Vector{0, 0}) {}
+
+  friend std::ostream& operator<<(std::ostream&, const Boid&);
 };
 
-struct World {
-  std::vector<Boid> boids;
-  size_t width;
-  size_t height;
+std::ostream& operator<<(std::ostream& os, const Boid& b)
+{
+  os << "position: " << b.position << " velocity: " << b.velocity;
+  return os;
+}
 
-  World(size_t n, size_t width, size_t height) : width(width), height(height) {
-    std::default_random_engine gen;
-    std::uniform_int_distribution<int> x_dist(0, width - 1);
-    std::uniform_int_distribution<int> y_dist(0, height - 1);
-    for (; n > 0; n--) {
-      boids.emplace_back(Vector{double(x_dist(gen)), double(y_dist(gen))});
-    }
-  }
+using Result = std::tuple<Vector, Vector, Vector>;
 
-  void draw_boids(sf::RenderWindow& window) {
-    for (Boid& boid: boids) {
-      sf::CircleShape shape(5.f, 3);
-      shape.setFillColor(sf::Color(0, 0, 0, 0));
-      shape.setOutlineThickness(1.f);
-      shape.setOutlineColor(sf::Color::Green);
-      shape.setPosition(boid.position.x, boid.position.y);
-      double r = atan2(boid.velocity.y, boid.velocity.x);
-      shape.setRotation(r * (180 / M_PI));
-      window.draw(shape);
-    }
-  }
+void old_step(const size_t width, const size_t height, cown_ptr<sf::RenderWindow> window, std::vector<cown_ptr<Boid>> boids, const size_t vlim) {
+  when() << [width, height, window, boids = std::move(boids), vlim]() mutable {
+    const size_t num_boids = boids.size();
 
-  Vector rule1(size_t idx) {
-    Vector pcj{0, 0};
+    std::vector<cown_ptr<Result>> results;
 
-    for (size_t i = 0; i < boids.size(); ++i) {
-      if (i != idx) {
-        pcj += boids[i].position;
-      }
-    }
-
-    pcj /= (boids.size() - 1);
-    return (pcj - boids[idx].position) / 50;
-  }
-
-  Vector rule2(size_t idx) {
-    Vector c{0, 0};
-
-    for (size_t i = 0; i < boids.size(); ++i) {
-      if (i != idx) {
-        Vector diff = boids[i].position - boids[idx].position;
-        if (diff.abs() < 30) {
-          c -= (diff / 2);
+    for (size_t i = 0; i < num_boids; ++i)
+    {
+      cown_ptr<Result> partial_result = make_cown<Result>(Vector{0, 0}, Vector{0, 0}, Vector{0, 0});
+      results.push_back(partial_result);
+      for (size_t j = 0; j < num_boids; ++j)
+      {
+        if (i != j) {
+          when(boids[i], boids[j], partial_result) << [] (acquired_cown< Boid> boid, acquired_cown< Boid> other, acquired_cown<Result> result){
+            std::get<0>(*result) += other->position;
+            Vector diff = other->position - boid->position;
+            if (diff.abs() < 30) std::get<1>(*result) -= (diff / 2);;
+            std::get<2>(*result) += other->velocity;
+          };
         }
       }
     }
 
-    return c;
+    for (size_t i = 0; i < num_boids; ++i)
+    {
+      when(boids[i], results[i]) << [num_boids, width, height, vlim](acquired_cown<Boid> boid, acquired_cown<Result> result){
+        std::get<0>(*result) /= (num_boids - 1);
+        std::get<0>(*result) = (std::get<0>(*result) - boid->position) / 50;
+
+        std::get<2>(*result) /= (num_boids - 1);
+        std::get<2>(*result) = (std::get<2>(*result) - boid->velocity) / 16;
+
+        Vector& p = boid->position;
+        Vector v4{0, 0};
+
+        if (p.x < 0) {
+          v4.x = 10;
+        } else if (p.x > width) {
+          v4.x = -10;
+        }
+
+        if (p.y < 0) {
+          v4.y = 10;
+        } else if (p.y > height) {
+          v4.y = -10;
+        }
+
+        Vector& v = boid->velocity;
+        v += std::get<0>(*result) + std::get<1>(*result) + std::get<2>(*result) + v4;
+        if (v.abs() > vlim) {
+          boid->velocity = (v / v.abs()) * vlim;
+        }
+        boid->position += v;
+      };
+    }
+
+    // this is a bit eager and will clear the window
+    // can we do a when<N> template?
+    // when(window) << [](acquired_cown<sf::RenderWindow> window) { window->clear(); };
+    for (size_t i = 0; i < num_boids; ++i)
+    {
+      when(read(boids[i]), window) << [i](acquired_cown<Boid const> boid, acquired_cown<sf::RenderWindow> window) {
+        std::cout << "boids[" << i << "]: " << *boid << std::endl;
+        sf::CircleShape shape(5.f, 3);
+        shape.setFillColor(sf::Color(0, 0, 0, 0));
+        shape.setOutlineThickness(1.f);
+        shape.setOutlineColor(sf::Color::Green);
+        shape.setPosition(boid->position.x, boid->position.y);
+        double r = atan2(boid->velocity.y, boid->velocity.x);
+        shape.setRotation(r * (180 / M_PI));
+        window->draw(shape);
+      };
+    }
+    when(window) << [](acquired_cown<sf::RenderWindow> window) { window->display(); };
+
+    old_step(width, height, window, std::move(boids), vlim);
+  };
+}
+
+void old_ran() {
+  const size_t width = 800;
+  const size_t height = 600;
+  cown_ptr<sf::RenderWindow> window = make_cown<sf::RenderWindow>(sf::VideoMode(width, height), "Boids");
+  const size_t num_boids = 50;
+  std::vector<cown_ptr<Boid>> boids;
+
+  std::default_random_engine gen;
+  std::uniform_int_distribution<int> x_dist(0, width - 1);
+  std::uniform_int_distribution<int> y_dist(0, height - 1);
+  for (size_t i = 0; i < num_boids; ++i) {
+    boids.push_back(make_cown<Boid>(Vector{double(x_dist(gen)), double(y_dist(gen))}));
   }
 
-  Vector rule3(size_t idx) {
-    Vector pvj{0, 0};
+  const int vlim = 10;
 
-    for (size_t i = 0; i < boids.size(); ++i) {
-      if (i != idx) {
-        pvj += boids[i].velocity;
+  old_step(width, height, window, std::move(boids), vlim);
+}
+
+template<typename C>
+static acquired_cown<C> cown_ptr_to_acquired(cown_ptr<C> c)
+{
+  return acquired_cown<C>(c);
+}
+
+template<size_t i, size_t j, typename... Ts>
+void compute_partial_result(acquired_cown<Result>& partial_result, std::tuple<Ts...>& boids) {
+  if constexpr (j >= sizeof...(Ts)) {
+    return;
+  } else if constexpr (i != j) {
+      std::get<0>(*partial_result) += std::get<j>(boids)->position;
+      Vector diff = std::get<j>(boids)->position - std::get<i>(boids)->position;
+      if (diff.abs() < 30) std::get<1>(*partial_result) -= (diff / 2);;
+      std::get<2>(*partial_result) += std::get<j>(boids)->velocity;
+  } else {
+    compute_partial_result<i, j+1>(partial_result, boids);
+  }
+}
+
+template<size_t i, typename... Ts>
+void compute_partial_results(std::vector<cown_ptr<Result>>& results, Ts... args) {
+  if constexpr (i >= sizeof...(Ts)) {
+    return;
+  } else {
+    cown_ptr<Result> partial_result = make_cown<Result>(Vector{0, 0}, Vector{0, 0}, Vector{0, 0});
+    results.push_back(partial_result);
+
+    when(partial_result, args...) << [](acquired_cown<Result> partial_result, decltype(cown_ptr_to_acquired(args))... acquired) {
+      const size_t num_boids = sizeof...(Ts);
+      std::tuple<decltype(cown_ptr_to_acquired(args))&...> boids(acquired...);
+      compute_partial_result<i, 0>(partial_result, boids);
+    };
+
+    compute_partial_results<i+1>(results, args...);
+  }
+}
+
+template<size_t i, typename... Ts>
+void update_boid_positions(std::vector<cown_ptr<Result>>& results, const size_t width, const size_t height, const size_t vlim, Ts... args) {
+  if constexpr (i >= sizeof...(Ts)) {
+    return;
+  } else {
+    std::tuple<Ts...> boids(args...);
+    when(std::get<i>(boids), results[i]) << [width, height, vlim](acquired_cown<Boid> boid, acquired_cown<Result> result){
+      const size_t num_boids = sizeof...(Ts);
+      std::get<0>(*result) /= (num_boids - 1);
+      std::get<0>(*result) = (std::get<0>(*result) - boid->position) / 50;
+
+      std::get<2>(*result) /= (num_boids - 1);
+      std::get<2>(*result) = (std::get<2>(*result) - boid->velocity) / 16;
+
+      Vector& p = boid->position;
+      Vector v4{0, 0};
+
+      if (p.x < 0) {
+        v4.x = 10;
+      } else if (p.x > width) {
+        v4.x = -10;
       }
-    }
 
-    pvj /= (boids.size() - 1);
+      if (p.y < 0) {
+        v4.y = 10;
+      } else if (p.y > height) {
+        v4.y = -10;
+      }
 
-    return (pvj - boids[idx].velocity) / 16;
+      Vector& v = boid->velocity;
+      v += std::get<0>(*result) + std::get<1>(*result) + std::get<2>(*result) + v4;
+      if (v.abs() > vlim) {
+        boid->velocity = (v / v.abs()) * vlim;
+      }
+      boid->position += v;
+    };
+    update_boid_positions<i + 1>(results, width, height, vlim, args...);
   }
+}
 
-  Vector bound_position(size_t idx) {
-    Boid& boid = boids[idx];
-    Vector& v = boid.position;
-
-    Vector r{0, 0};
-
-    if (v.x < 0) {
-      r.x = 10;
-    } else if (v.x > width) {
-      r.x = -10;
-    }
-
-    if (v.y < 0) {
-      r.y = 10;
-    } else if (v.y > height) {
-      r.y = -10;
-    }
-
-    return r;
+template<size_t i, typename... Ts>
+void draw_boids(acquired_cown<sf::RenderWindow>& window, std::tuple<Ts...>& boids) {
+  if constexpr (i >= sizeof...(Ts)) {
+    return;
+  } else {
+    //     std::cout << "boids[" << i << "]: " << *boid << std::endl;
+    acquired_cown<Boid>& boid = std::get<i>(boids);
+    sf::CircleShape shape(5.f, 3);
+    shape.setFillColor(sf::Color(0, 0, 0, 0));
+    shape.setOutlineThickness(1.f);
+    shape.setOutlineColor(sf::Color::Green);
+    shape.setPosition(boid->position.x, boid->position.y);
+    double r = atan2(boid->velocity.y, boid->velocity.x);
+    shape.setRotation(r * (180 / M_PI));
+    window->draw(shape);
+    draw_boids<i + 1>(window, boids);
   }
+}
 
-  void limit_velocity(Boid& boid) {
-    static const double vlim = 10;
-    Vector& v = boid.velocity;
-    std::cout << "v = " << v << " |v|= " << v.abs() << std::endl;
-    if (v.abs() > vlim) {
-      std::cout << "lim" << std::endl;
-      std::cout << "(v / v.abs())" << (v / v.abs()) << std::endl;
-      std::cout << "(v / v.abs()) * vlim" << (v / v.abs()) * vlim << std::endl;
-      boid.velocity = (v / v.abs()) * vlim;
-    }
-  }
+template<typename... Ts>
+void step(const size_t width, const size_t height, cown_ptr<sf::RenderWindow> window, const size_t vlim, Ts... args) {
+  when() << [width, height, window, vlim, args...]() mutable {
+    const size_t num_boids = sizeof...(Ts);
 
-  void move_all_boids() {
-    Vector v1, v2, v3, v4;
-    for (size_t i = 0; i < boids.size(); ++i) {
-      v1 = rule1(i);
-      std::cout << "Boid " << i << " v1: {" << v1.x << ", " << v1.y << "}" << std::endl;
-      v2 = rule2(i);
-      std::cout << "Boid " << i << " v2: {" << v2.x << ", " << v2.y << "}" << std::endl;
-      v3 = rule3(i);
-      std::cout << "Boid " << i << " v3: {" << v3.x << ", " << v3.y << "}" << std::endl;
-      v4 = bound_position(i);
-      std::cout << "Boid " << i << " v4: " << v4 << std::endl;
+    std::vector<cown_ptr<Result>> results;
+    compute_partial_results<0>(results, args...);
 
+    update_boid_positions<0>(results, width, height, vlim, args...);
 
-      Boid& boid = boids[i];
-      boid.velocity += v1 + v2 + v3 + v4;
-      limit_velocity(boid);
-      boid.position += boid.velocity;
-      std::cout << "Boid " << i << ": " << boid.position << " " << boid.velocity << std::endl;
-    }
-    std::cout << "--------------" << std::endl;
-  }
-};
+    when(window, args...) << [](acquired_cown<sf::RenderWindow> window, decltype(cown_ptr_to_acquired(args))... acquired) {
+      window->clear();
+      std::tuple<decltype(cown_ptr_to_acquired(args))&...> boids(acquired...);
+      draw_boids<0>(window, boids);
+      window->display();
+    };
 
-  // void run() {
-  //   while(true) {
-  //     draw_boids();
-  //     move_all_boids();
-  //   }
+    step(width, height, window, vlim, args...);
+  };
+}
+
+void run() {
+  const size_t width = 800;
+  const size_t height = 600;
+  cown_ptr<sf::RenderWindow> window = make_cown<sf::RenderWindow>(sf::VideoMode(width, height), "Boids");
+  // const size_t num_boids = 5;
+  // std::vector<cown_ptr<Boid>> boids;
+
+  std::default_random_engine gen;
+  std::uniform_int_distribution<int> x_dist(0, width - 1);
+  std::uniform_int_distribution<int> y_dist(0, height - 1);
+  // for (size_t i = 0; i < num_boids; ++i) {
+    // boids.push_back(make_cown<Boid>(Vector{double(x_dist(gen)), double(y_dist(gen))}));
   // }
+
+  const int vlim = 10;
+
+  step(width, height, window, vlim,
+    make_cown<Boid>(Vector{double(x_dist(gen)), double(y_dist(gen))}),
+    make_cown<Boid>(Vector{double(x_dist(gen)), double(y_dist(gen))}),
+    make_cown<Boid>(Vector{double(x_dist(gen)), double(y_dist(gen))}),
+    make_cown<Boid>(Vector{double(x_dist(gen)), double(y_dist(gen))}));
+}
+
+void something(const size_t width, const size_t height, cown_ptr<sf::RenderWindow> window, const size_t vlim) {
+  cown_ptr<Boid> a = make_cown<Boid>(Vector{10, 10});
+  cown_ptr<Boid> b = make_cown<Boid>(Vector{20, 20});
+
+  step(width, height, window, vlim, a, b);
+}
 
 int main(int argc, char** argv)
 {
-    size_t width = 800;
-  size_t height = 600;
-  sf::RenderWindow window(sf::VideoMode(width, height), "Boids");
-
-  World world(10, width, height);
-
-  //  n boid cowns
-  //  n resulting velocity cowns
-  // while(true) {
-  //  for result in results
-  //    when(read(boids), result) { result = compute new boid velocity }
-  //  for boid, result in zip(boids, results)
-  //    when(boid, result) { boid.velocity = result; boid.position += boid.velocity }
-  //  when(read(boids), window) { draw all boids }
-  // }
-
-  while (window.isOpen())
-  {
-      sf::Event event;
-      while (window.pollEvent(event))
-      {
-          if (event.type == sf::Event::Closed)
-              window.close();
-      }
-
-      window.clear();
-
-      world.draw_boids(window);
-      world.move_all_boids();
-
-      window.display();
-  }
-
-  // SystematicTestHarness harness(argc, argv);
+  SystematicTestHarness harness(argc, argv);
+  harness.run(run);
 }
